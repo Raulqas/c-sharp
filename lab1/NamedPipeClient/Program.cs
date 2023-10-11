cfe4142dc5798395a3e98126eb5d64ee3f97be64
gasanovvv <89770304+Raulqas@users.noreply.github.com>
2023-10-11 14:23:32 +0500

    Add files via upload

---
+++ lab1/NamedPipeClient/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Client
+{
+    class Program
+    {
+        public struct Ad
+        {
+            public int X;
+            public int Y;
+            public bool DA;
+        }
+
+        static async Task Main()
+        {
+            Console.WriteLine("Соединяю с сервером...\n");
+
+            using (var clientPipe = new NamedPipeClientStream(".", "tonel", PipeDirection.InOut, PipeOptions.Asynchronous))
+            {
+                await clientPipe.ConnectAsync();
+
+                Console.WriteLine("Соединение установлено, отправляем данные...\n");
+
+                Ad dataToSend = new Ad
+                {
+                    X = 100,
+                    Y = 150,
+                    DA = false
+                };
+
+                SendData(clientPipe, dataToSend);
+
+                Console.WriteLine($"Отправлены данные: X={dataToSend.X}, Y={dataToSend.Y}, DA={dataToSend.DA}\n");
+
+                byte[] buffer = new byte[Marshal.SizeOf<Ad>()];
+                await clientPipe.ReadAsync(buffer, 0, buffer.Length);
+
+                Ad receivedData = MemoryMarshal.Read<Ad>(buffer);
+
+                Console.WriteLine($"Получены данные от сервера: X={receivedData.X}, Y={receivedData.Y}, DA={receivedData.DA}\n");
+            }
+
+            Console.WriteLine("Клиент завершил работу.");
+        }
+
+        static void SendData(NamedPipeClientStream pipe, Ad data)
+        {
+            byte[] buffer = new byte[Marshal.SizeOf<Ad>()];
+            MemoryMarshal.Write(buffer, ref data);
+            pipe.Write(buffer, 0, buffer.Length);
+        }
+    }
+}
---
+++ lab1/NamedPipeServer/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Server
+{
+    class Program
+    {
+        public struct Ad
+        {
+            public int X;
+            public int Y;
+            public bool DA;
+        }
+        static async Task Main()
+        {
+            Console.WriteLine("Ожидание клиента...\n");
+
+            using (var serverPipe = new NamedPipeServerStream("tonel", PipeDirection.InOut, NamedPipeServerStream.MaxAllowedServerInstances, PipeTransmissionMode.Byte, PipeOptions.Asynchronous))
+            {
+                await serverPipe.WaitForConnectionAsync();
+
+                Console.WriteLine("Клиент подключен.\n");
+
+                Ad receivedData = ReceiveData(serverPipe);
+                Console.WriteLine($"Получены данные от клиента: X={receivedData.X}, Y={receivedData.Y}, DA={receivedData.DA}\n");
+
+                receivedData.X += receivedData.Y;
+                receivedData.Y -= receivedData.X;
+                receivedData.DA = true;
+
+                Console.WriteLine($"Отправляем данные клиенту: X={receivedData.X}, Y={receivedData.Y}, dA={receivedData.DA}\n");
+                SendData(serverPipe, receivedData);
+            }
+
+            Console.WriteLine("Сервер завершил работу.");
+        }
+        static Ad ReceiveData(NamedPipeServerStream pipe)
+        {
+            byte[] buffer = new byte[Marshal.SizeOf<Ad>()];
+            pipe.Read(buffer, 0, buffer.Length);
+            return MemoryMarshal.Read<Ad>(buffer);
+        }
+
+        static void SendData(NamedPipeServerStream pipe, Ad data)
+        {
+            byte[] buffer = new byte[Marshal.SizeOf<Ad>()];
+            MemoryMarshal.Write(buffer, ref data);
+            pipe.Write(buffer, 0, buffer.Length);
+        }
+
+        
+    }
+}
